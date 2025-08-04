dbe4c76a99a867f5143e1ef05959897a398c0887
Ala <alahrytsenka@gmail.com>
2025-08-05 00:24:34 +0300

    HW-3

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,13 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+let n = 1;
+let nn = String(n) + String(n);
+let nnn = String(n) + String(n) + String(n);
+console.log(n + +nn + +nnn);
+
+
+or 
+let m = 2;
+let result = m + +(String(m) + String(m)) + +(String(m) + String(m) + String(m));
+console.log(result);
