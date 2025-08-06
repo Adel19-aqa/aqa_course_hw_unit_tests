caf14d21ae877a0103044e2ccca8ef9a3ec68f72
Ala <alahrytsenka@gmail.com>
2025-08-07 01:21:32 +0300

    third solution for the task

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -10,9 +10,18 @@ let nnn = String(n) + String(n) + String(n);
 n + nn + nnn
 //console.log(n + +nn + +nnn);
 
-
 //or 
 let m = 2;
 let result = m + +(String(m) + String(m)) + +(String(m) + String(m) + String(m));
 m + mm + mmm
 //console.log(result);
+
+
+const y = 3;
+if (y < 1) {
+ console.log("Choose 0 < y < 10");
+} else if (y > 10) {
+    console.log("Choose 0 < y < 10");
+} else {
+    console.log(y + +(String(y) + String(y)) + +(String(y) + String(y) + String(y)));
+}
